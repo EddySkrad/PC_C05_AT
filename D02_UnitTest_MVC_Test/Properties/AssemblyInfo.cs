4be513b649d322762c1930348d535a869df2aedf
Filipe Cruz <f.r.cruz@hotmail.com>
2022-01-07 10:54:01 +0000

    Add project files.

---
+++ D02_UnitTest_MVC_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("D02_UnitTest_MVC_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("D02_UnitTest_MVC_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0be966ea-d0da-4d65-a6e0-ee8667e88fea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ D02_UnitTest_MVC_Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+using D02_UnitTest_MVC_Client.Controllers;
+
+namespace D02_UnitTest_MVC_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AboutAction_ShouldReturnAboutView_Error()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("About", result.ViewName);
+
+        }
+    }
+}
